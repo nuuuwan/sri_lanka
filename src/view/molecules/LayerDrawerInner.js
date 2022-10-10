e3ae061f9c4041ececa69ca3258a2ba27788fb87
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-10 16:07:03 +0530

    refactor: Simplified

--- src/view/molecules/LayerDrawerInner.js
+++ src/view/molecules/LayerDrawerInner.js
@@ -1,17 +1,3 @@
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-
-const STYLE_BOX = {
-  width: 320,
-  maxWidth: "80%",
-};
-export default function LayerDrawerInner({ handleCloseLayerDrawer }) {
-  return (
-    <Box sx={STYLE_BOX}>
-      <IconButton onClick={handleCloseLayerDrawer}>
-        <CloseIcon />
-      </IconButton>
-    </Box>
-  );
+export default function LayerDrawerInner() {
+  return "LayerDrawerInner";
 }
