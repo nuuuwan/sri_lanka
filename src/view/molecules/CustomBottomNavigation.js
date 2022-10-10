5fb8e14a9525edd844ee88b245da54ff8a114e86
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-10 15:04:21 +0530

    feat: Added handleClickOpenLayer

--- src/view/molecules/CustomBottomNavigation.js
+++ src/view/molecules/CustomBottomNavigation.js
@@ -2,10 +2,13 @@ import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import LayersIcon from "@mui/icons-material/Layers";
 
-export default function CustomBottomNavigation() {
+export default function CustomBottomNavigation({ handleClickOpenLayer }) {
   return (
     <BottomNavigation>
-      <BottomNavigationAction icon={<LayersIcon />} />
+      <BottomNavigationAction
+        icon={<LayersIcon />}
+        onClick={handleClickOpenLayer}
+      />
     </BottomNavigation>
   );
 }
