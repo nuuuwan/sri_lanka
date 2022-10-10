aca6708d0d4fa99850d24520afbe6210d065098b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-10 11:08:05 +0530

    [fix] renderCustom BUG

--- src/view/molecules/RegionGeo.js
+++ src/view/molecules/RegionGeo.js
@@ -102,7 +102,7 @@ export default class RegionGeo extends Component {
             </h2>
             {buttonShow}
             <hr />
-            {renderCustom(regionID, iRegion)}
+            {renderCustom ? renderCustom(regionID, iRegion) : null}
           </Popup>
         </GeoJSON>
       </>
