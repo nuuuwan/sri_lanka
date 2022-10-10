7284f65ff33e51f64565b17a1958fda6b914bd05
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-10 16:06:32 +0530

    refactor: handleClickOpenLayer -> handleOpenLayerDrawer

--- src/view/molecules/CustomBottomNavigation.js
+++ src/view/molecules/CustomBottomNavigation.js
@@ -2,12 +2,12 @@ import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import LayersIcon from "@mui/icons-material/Layers";
 
-export default function CustomBottomNavigation({ handleClickOpenLayer }) {
+export default function CustomBottomNavigation({ handleOpenLayerDrawer }) {
   return (
     <BottomNavigation>
       <BottomNavigationAction
         icon={<LayersIcon />}
-        onClick={handleClickOpenLayer}
+        onClick={handleOpenLayerDrawer}
       />
     </BottomNavigation>
   );
