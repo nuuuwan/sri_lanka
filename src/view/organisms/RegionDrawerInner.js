1cb77f01a97007a5e5c698976b2c7442de3e7dfd
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-10 17:00:25 +0530

    feat: load GIG2 table

--- src/view/organisms/RegionDrawerInner.js
+++ src/view/organisms/RegionDrawerInner.js
@@ -1,9 +1,39 @@
 import { Component } from "react";
 
+import Box from "@mui/material/Box";
+
+import GIG2 from "../../nonview/base/GIG2";
+
 import EntView from "../../view/atoms/EntView";
 
 export default class RegionDrawerInner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { table: undefined };
+  }
+
+  async componentDidMount() {
+    const { selectedLayerTableName, selectedRegionID } = this.props;
+    if (!selectedLayerTableName || !selectedRegionID) {
+      return;
+    }
+
+    const tableIndex = await GIG2.getTableIndex(selectedLayerTableName);
+    const table = tableIndex[selectedRegionID];
+    this.setState({ table });
+  }
+
   render() {
-    return <EntView entID={this.props.selectedRegionID} />;
+    const { table } = this.state;
+    if (!table) {
+      return "Loading...";
+    }
+
+    return (
+      <Box>
+        <EntView entID={this.props.selectedRegionID} />
+        {JSON.stringify(table)}
+      </Box>
+    );
   }
 }
