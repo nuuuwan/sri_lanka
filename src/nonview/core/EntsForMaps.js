35ecf63154324b7237522c229e8308c13cd181d8
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-11 14:48:17 +0530

    feat: smarter getDisplayRegionIDs, which consider the actual distance as opposed to just ranked order

--- src/nonview/core/EntsForMaps.js
+++ src/nonview/core/EntsForMaps.js
@@ -1,20 +1,9 @@
 import { ENT } from "../../nonview/base/Ents";
 
-const N_DISPLAY_REGIONS = 30;
-export default class EntsForMaps {
-  static getEntTypeForZoom(zoom) {
-    if (zoom <= 7) {
-      return ENT.PROVINCE;
-    }
-    if (zoom <= 11) {
-      return ENT.DISTRICT;
-    }
-    if (zoom <= 14) {
-      return ENT.DSD;
-    }
-    return ENT.GND;
-  }
+const MAX_DISPLAY_REGIONS = 60;
+const K_MAX_DISTANCE = 2000;
 
+export default class EntsForMaps {
   static getDistance([latCenter, lngCenter], { centroid }) {
     const [lat, lng] = JSON.parse(centroid);
     const [dlat, dlng] = [lat - latCenter, lng - lngCenter];
@@ -23,18 +12,30 @@ export default class EntsForMaps {
   }
 
   static getDisplayRegionIDs(allEntIndex, center, zoom) {
-    const entType = this.getEntTypeForZoom(zoom);
-    const entIndex = allEntIndex[entType];
-    const sortedEnts = Object.values(entIndex).sort(function (entA, entB) {
-      return (
-        EntsForMaps.getDistance(center, entA) -
-        EntsForMaps.getDistance(center, entB)
-      );
-    });
+    const maxDistance = K_MAX_DISTANCE / Math.pow(2, zoom);
 
-    const displayRegionIDs = sortedEnts
-      .slice(0, N_DISPLAY_REGIONS)
-      .map((ent) => ent.id);
-    return displayRegionIDs;
+    for (let entType of [ENT.GND, ENT.DSD, ENT.DISTRICT, ENT.PROVINCE]) {
+      const entIndex = allEntIndex[entType];
+      const displayEnts = Object.values(entIndex)
+        .filter(function (ent) {
+          const distance = EntsForMaps.getDistance(center, ent);
+          return distance < maxDistance;
+        })
+        .sort(function (entA, entB) {
+          return (
+            EntsForMaps.getDistance(center, entA) -
+            EntsForMaps.getDistance(center, entB)
+          );
+        });
+
+      const nDisplayEnts = displayEnts.length;
+      if (nDisplayEnts < MAX_DISPLAY_REGIONS) {
+        const displayRegionIDs = displayEnts
+          .slice(0, MAX_DISPLAY_REGIONS)
+          .map((ent) => ent.id);
+        return displayRegionIDs;
+      }
+    }
+    return [];
   }
 }
