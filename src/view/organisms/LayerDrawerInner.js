09238d66abff3ef68e7faf6c6b2865b2c4bf55f7
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-12 15:59:29 +0530

    refactor: Use TableTitleView

--- src/view/organisms/LayerDrawerInner.js
+++ src/view/organisms/LayerDrawerInner.js
@@ -7,34 +7,18 @@ import GIG2 from "../../nonview/base/GIG2";
 import LayerView from "../../view/molecules/LayerView";
 
 export default class LayerDrawInner extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { metaData: null };
-  }
-
-  async componentDidMount() {
-    const metaData = (await GIG2.getMetaData()).filter(function (metaDatum) {
-      return metaDatum.table_name.substring(0, 6) === "region";
-    });
-    this.setState({ metaData });
-  }
-
   render() {
-    const { metaData } = this.state;
-    if (!metaData) {
-      return "Loading...";
-    }
-
     const { selectedLayerTableName, setSelectedLayerTableName } = this.props;
+    const tableNames = GIG2.getTableNames();
 
     return (
       <List>
-        {metaData.map(function (layerData, iLayer) {
-          const key = "layer-" + iLayer;
+        {tableNames.map(function (tableName) {
+          const key = "table-" + tableName;
           return (
             <LayerView
               key={key}
-              layerData={layerData}
+              tableName={tableName}
               selectedLayerTableName={selectedLayerTableName}
               setSelectedLayerTableName={setSelectedLayerTableName}
             />
