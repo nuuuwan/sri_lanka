36cda93219540857af7da076c18ef76f546cd960
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-10-21 09:15:49 +0530

    perf: Access geo from outside

--- src/nonview/base/GeoData.js
+++ src/nonview/base/GeoData.js
@@ -1,10 +1,13 @@
 import Ents from "./Ents.js";
 import WWW from "./WWW.js";
 
+const URL_BASE =
+  "https://raw.githubusercontent.com/nuuuwan/gig-data/master/geo";
+
 export default class GeoData {
   static async getGeoForRegion(regionID) {
     const regionType = Ents.getEntType(regionID);
-    const url = `/sri_lanka/data/geo/${regionType}/${regionID}.json`;
-    return await WWW.json(url);
+    const url = URL_BASE + `/${regionType}/${regionID}.json`;
+    return [await WWW.json(url)];
   }
 }
