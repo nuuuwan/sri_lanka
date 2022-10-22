1a2f04261b5ca6f903c58cf19cd945c4673d530a
nuuuwan <nuuuwan@gmail.com>
2022-10-22 09:48:17 +0530

    hack: disable getLatLng

--- src/nonview/base/GeoLocation.js
+++ src/nonview/base/GeoLocation.js
@@ -1,12 +1,13 @@
 export default class GeoLocation {
   static async getLatLng() {
-    const position = await new Promise((resolve, reject) =>
-      navigator.geolocation.getCurrentPosition(resolve, reject)
-    );
-    if (!position) {
-      return null;
-    }
-    const coords = position.coords;
-    return [coords.latitude, coords.longitude];
+    return null;
+    // const position = await new Promise((resolve, reject) =>
+    //   navigator.geolocation.getCurrentPosition(resolve, reject)
+    // );
+    // if (!position) {
+    //   return null;
+    // }
+    // const coords = position.coords;
+    // return [coords.latitude, coords.longitude];
   }
 }
