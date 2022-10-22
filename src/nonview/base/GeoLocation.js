64b39c5b14e47c8df077491f1ddfe6d135f5fbfe
nuuuwan <nuuuwan@gmail.com>
2022-10-22 09:56:15 +0530

    refactor: Renabled getLatLng; Added try/catch and options

--- src/nonview/base/GeoLocation.js
+++ src/nonview/base/GeoLocation.js
@@ -1,13 +1,26 @@
+const OPTIONS = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
 export default class GeoLocation {
+  static async getPosition() {
+    return new Promise((resolve, reject) =>
+      navigator.geolocation.getCurrentPosition(resolve, reject, OPTIONS)
+    );
+  }
   static async getLatLng() {
-    return null;
-    // const position = await new Promise((resolve, reject) =>
-    //   navigator.geolocation.getCurrentPosition(resolve, reject)
-    // );
-    // if (!position) {
-    //   return null;
-    // }
-    // const coords = position.coords;
-    // return [coords.latitude, coords.longitude];
+    let position;
+    try {
+      position = await GeoLocation.getPosition();
+    } catch (error) {
+      return null;
+    }
+    if (!position) {
+      return null;
+    }
+    const coords = position.coords;
+    return [coords.latitude, coords.longitude];
   }
 }
