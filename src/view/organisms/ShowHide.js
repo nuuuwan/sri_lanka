7369c1a0acc5a9e751ddd7a631b4a064641816bf
nuuuwan <nuuuwan@gmail.com>
2022-10-23 08:42:26 +0530

    feat: + ShowHide

---
+++ src/view/organisms/ShowHide.js
@@ -0,0 +1,51 @@
+import { Component } from "react";
+
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+const STYLE_BUTTON = {
+  fontSize: "50%",
+  p: 0,
+  m: 0,
+};
+
+export default class ShowHide extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+  }
+
+  renderWhenHide() {
+    const onClick = function () {
+      this.setState({ show: true });
+    }.bind(this);
+    return (
+      <Button onClick={onClick} sx={STYLE_BUTTON}>
+        {this.props.title}
+      </Button>
+    );
+  }
+
+  renderWhenShow() {
+    const onClick = function () {
+      this.setState({ show: false });
+    }.bind(this);
+    return (
+      <>
+        <Button onClick={onClick} sx={STYLE_BUTTON}>
+          {"X"}
+        </Button>
+        {this.props.children}
+      </>
+    );
+  }
+
+  render() {
+    return (
+      <Stack direction="row" alignItems="right">
+        {this.state.show ? this.renderWhenShow() : this.renderWhenHide()}
+      </Stack>
+    );
+  }
+}
