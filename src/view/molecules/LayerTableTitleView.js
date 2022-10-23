34329bca03665cfd83a0e9cd15cbe6f2baf375fc
nuuuwan <nuuuwan@gmail.com>
2022-10-23 11:32:23 +0530

    style: Added missing icons

--- src/view/molecules/LayerTableTitleView.js
+++ src/view/molecules/LayerTableTitleView.js
@@ -3,48 +3,29 @@ import Box from "@mui/material/Box";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import DeleteIcon from "@mui/icons-material/Delete";
-import HouseIcon from "@mui/icons-material/House";
 import HowToVoteIcon from "@mui/icons-material/HowToVote";
 import LayersIcon from "@mui/icons-material/Layers";
-import LightbulbIcon from "@mui/icons-material/Lightbulb";
-import LocalDrinkIcon from "@mui/icons-material/LocalDrink";
-import TempleBuddhistIcon from "@mui/icons-material/TempleBuddhist";
-import WcIcon from "@mui/icons-material/Wc";
 
 import GIG2TableMetadata from "../../nonview/base/GIG2TableMetadata";
+import MEASUREMENT_TO_ICON from "../../nonview/constants/MEASUREMENT_TO_ICON";
 
 const STYLE_BODY1 = {
   fontSize: "50%",
 };
 
+const DEFAULT_ICON = LayersIcon;
+
 function getTableIcon(tableMetadata) {
-  if (tableMetadata.entity === "regions_ec") {
+  if (tableMetadata.entity === "regions-ec") {
     return HowToVoteIcon;
   }
 
-  if (tableMetadata.measurement === "") {
-    return LocalDrinkIcon;
+  let Icon = MEASUREMENT_TO_ICON[tableMetadata.measurement];
+  if (Icon) {
+    return Icon;
   }
-
-  switch (tableMetadata.measurement) {
-    case "population-ethnicity":
-      return TempleBuddhistIcon;
-    case "population-religion":
-      return TempleBuddhistIcon;
-    case "social-household-lighting":
-      return LightbulbIcon;
-    case "social-household-waste_disposal":
-      return DeleteIcon;
-    case "social-household-drinking_water":
-      return LocalDrinkIcon;
-    case "social-household-toilet_facilities":
-      return WcIcon;
-    case "social-household-year_of_construction":
-      return HouseIcon;
-    default:
-      return LayersIcon;
-  }
+  console.debug(tableMetadata.measurement);
+  return DEFAULT_ICON;
 }
 
 export default function LayerTableTitleView({ tableName }) {
