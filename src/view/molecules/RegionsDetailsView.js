50be692cf2436872efe1bbb452a0313288ab06c9
nuuuwan <nuuuwan@gmail.com>
2022-10-25 20:33:37 +0530

    feat: use RegionsTableView

--- src/view/molecules/RegionsDetailsView.js
+++ src/view/molecules/RegionsDetailsView.js
@@ -1,31 +1,43 @@
 import Box from "@mui/material/Box";
 
+import RegionsTableView from "../../view/molecules/RegionsTableView";
+
 const STYLE_BOX = {
   marginLeft: 1,
   marginRight: 1,
   marginBottom: 1,
+  width: 200,
+  height: 200,
+  overflow: "scroll",
 };
-
 
 export default function RegionsDetailsView({
   layerTable,
   layerTableName,
-  regionID,
+  displayRegionIDs,
   setColoringMethod,
   coloringMethod,
+  regionID,
 }) {
   if (!layerTable) {
     return null;
   }
-  const tableRow = layerTable.getRowByID(regionID);
-
   const onClickMajority = function () {
     setColoringMethod("majority");
   };
 
   return (
     <Box sx={STYLE_BOX}>
-
+      <RegionsTableView
+        key={`regions-table-${layerTableName}`}
+        coloringMethod={coloringMethod}
+        onClickMajority={onClickMajority}
+        regionID={regionID}
+        layerTable={layerTable}
+        layerTableName={layerTableName}
+        displayRegionIDs={displayRegionIDs}
+        setColoringMethod={setColoringMethod}
+      />
     </Box>
   );
 }
