26569248f06c21f2d5681848a5618e503e4f15b2
nuuuwan <nuuuwan@gmail.com>
2022-10-25 18:03:37 +0530

    feat: AllDetailsView stub

---
+++ src/view/organisms/AllDetailsView.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class AllDetailsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {}
+
+  render() {
+    return "TODO-AllDetailsView";
+  }
+}
