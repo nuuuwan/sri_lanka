62263f8fe1774d4e46d6c92a50496e6bb4b11a47
nuuuwan <nuuuwan@gmail.com>
2022-11-03 16:30:27 +0530

    refac: Added POLITICAL_PARTY_TO_COLOR

--- src/nonview/constants/POLITICAL_PARTY_TO_COLOR.js
+++ src/nonview/constants/POLITICAL_PARTY_TO_COLOR.js
@@ -1,15 +1,22 @@
-const POLITICAL_PARTY_TO_COLOR = {
-  SLFP: "blue",
-  PA: "blue",
-  UPFA: "blue",
-  SLPP: "maroon",
-  UNP: "green",
-  NDF: "green",
-  JVP: "red",
-  LSSP: "red",
-  ACTC: "orange",
-  SLMP: "purple",
-  NMPP: "red",
-  NPP: "red",
+const COLOR_TO_POLITICAL_PARTIES = {
+  blue: ["SLFP", "PA", "UPFA"],
+  green: ["UNP", "NDF"],
+  lightgreen: ["SJB"],
+  maroon: ["SLPP"],
+  orange: ["ACTC"],
+  pink: ["NPP"],
+  red: ["JVP", "LSSP", "EPDP", "NMPP", "JJB", "TMVP"],
+  yellow: ["ITAK", "AITC"],
+  darkgreen: ["MNA", "SLMC"],
 };
+
+const POLITICAL_PARTY_TO_COLOR = Object.entries(
+  COLOR_TO_POLITICAL_PARTIES
+).reduce(function (COLOR_TO_POLITICAL_PARTIES, [color, polical_parties]) {
+  for (let political_party of polical_parties) {
+    COLOR_TO_POLITICAL_PARTIES[political_party] = color;
+  }
+  return COLOR_TO_POLITICAL_PARTIES;
+}, {});
+
 export default POLITICAL_PARTY_TO_COLOR;
