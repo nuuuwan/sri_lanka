da692914effbe86384fb3041636617b12f55ccb4
nuuuwan <nuuuwan@gmail.com>
2022-11-12 18:10:45 +0530

    feat: Added refresh and TODO back

--- src/view/molecules/CustomBottomNavigation.js
+++ src/view/molecules/CustomBottomNavigation.js
@@ -1,12 +1,32 @@
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import RefreshIcon from '@mui/icons-material/Refresh';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 export default function CustomBottomNavigation({
   onClickCenterOnCurrentLocation,
 }) {
+
+  const onClickRefresh = function() {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  const onClickBack = function() {
+    console.debug('TODO');
+  }
+
   return (
     <BottomNavigation>
+      <BottomNavigationAction
+        icon={<ArrowBackIosIcon />}
+        onClick={onClickBack}
+      />
+      <BottomNavigationAction
+        icon={<RefreshIcon />}
+        onClick={onClickRefresh}
+      />
       <BottomNavigationAction
         icon={<LocationOnIcon />}
         onClick={onClickCenterOnCurrentLocation}
