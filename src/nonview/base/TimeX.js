5fe45b80525452454a0ce378c63b491c20cad072
nuuuwan <nuuuwan@gmail.com>
2022-11-12 16:57:55 +0530

    feat: + getUnixTimeMillisecond

--- src/nonview/base/TimeX.js
+++ src/nonview/base/TimeX.js
@@ -3,6 +3,11 @@ export default class TimeX {
     return Math.floor(Date.now() / 1000);
   }
 
+  static getUnixTimeMillisecond() {
+    return Math.floor(Date.now());
+  }
+
+
   static getDate() {
     return new Date().toISOString().slice(0, 10);
   }
